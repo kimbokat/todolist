a6bc69e86fe22dea440741a3d533ad053c8684aa
kimbokat <90966436+kimbokat@users.noreply.github.com>
2023-03-27 12:54:35 +0300

    MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 import Todolist from "./components/Todolist";
 
 function App() {
  
-  return <Todolist />
+  return (
+  <div className="App">
+
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6">
+        My To Do Lists
+      </Typography>
+    </Toolbar>
+  </AppBar>
+ 
+ <Todolist />
+  </div>
+  );
  
 }
 
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,65 +1,120 @@
 import React from "react";
-import { AgGridReact } from 'ag-grid-react';
-import { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { useState, useRef } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-material.css';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 export default function Todolist() {
   const [todo, setTodo] = useState({ desc: "", date: "", priority: "" });
   const [todos, setTodos] = useState([]);
 
+  const gridRef = useRef();
 
-  
-    const addTodo = (event) => {
-      event.preventDefault();
-      setTodos([...todos, todo]);
-    };
-  
-    const inputChanged = (event) => {
-      setTodo({ ...todo, [event.target.name]: event.target.value });
-    };
-  
-    const [columnDefs] = useState([
-      {field: 'desc', sortable: true, filter: true},
-      {field: 'priority', sortable: true, filter: true,
-        cellStyle: params => params.value === ("High", "high") ? {color: 'red'} : {color: 'black'}
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({ description: "", date: "" });
+  };
+
+  const deleteTodo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0)
+      setTodos(
+        todos.filter(
+          (todo, index) => gridRef.current.getSelectedNodes()[0].id != index
+        )
+      );
+    else alert("Please select a row first");
+  };
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  const [columnDefs] = useState([
+    { field: "desc", sortable: true, filter: true },
+    {
+      field: "priority",
+      sortable: true,
+      filter: true,
+      cellStyle: (params) =>
+        params.value === "high" || "High"
+          ? { color: "red" }
+          : { color: "black" },
     },
-      {field: 'date', sortable: true, filter: true}
-    ])
+    { field: "date", sortable: true, filter: true },
+  ]);
 
   return (
     <div className="App">
-      <form onSubmit={addTodo}>
-        <input
-          type="date"
+      
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <TextField
+          variant="standard"
+          label="date"
           name="date"
           value={todo.date}
           onChange={inputChanged}
         />
-        <input
-          placeholder="description"
+        <TextField
+          variant="standard"
+          label="description"
           type="text"
           name="desc"
           value={todo.desc}
           onChange={inputChanged}
         />
-         <input
-         placeholder="priority"
+        <TextField
+          variant="standard"
+          label="priority"
           type="text"
           name="priority"
           value={todo.priority}
           onChange={inputChanged}
         />
-        <input type="submit" value="Add" />
-        <div className='ag-theme-material' style={{height: 500, width: 600, margin: 'auto'}}>
+        <Button
+          size="small"
+          startIcon={<AddIcon />}
+          variant="contained"
+          onClick={addTodo}
+        >
+          Add todo
+        </Button>
+        <Button
+          size="small"
+          startIcon={<DeleteIcon />}
+          variant="contained"
+          color="error"
+          onClick={deleteTodo}
+        >
+          Delete
+        </Button>
+      </Stack>
+
+      <div
+        className="ag-theme-material"
+        style={{ height: 500, width: 600, margin: "auto" }}
+      >
         <AgGridReact
-               rowData={todos}
-               columnDefs={columnDefs}>
-           </AgGridReact>
-        </div>
-      </form>
-    {/*<Todotable todos={todos} setTodos={setTodos} />*/}
+          ref={gridRef}
+          onGridReady={(params) => (gridRef.current = params.api)}
+          rowData={todos}
+          columnDefs={columnDefs}
+          rowSelection="single"
+        ></AgGridReact>
+      </div>
+
+      {/*<Todotable todos={todos} setTodos={setTodos} />*/}
     </div>
   );
 }
