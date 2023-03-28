89803cd7bd8a0feeeb9f3655a5ba67a01ea3e5d8
Kim <bgx170@myy.haaga-helia.fi>
2023-03-28 22:44:53 +0300

    MUI picker

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,4 +1,5 @@
 import React from "react";
+import dayjs from "dayjs"; //mahdollinen ratkaisu, tutki lisää
 import { AgGridReact } from "ag-grid-react";
 import { useState, useRef } from "react";
 import Button from "@mui/material/Button";
@@ -7,11 +8,15 @@ import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 
 export default function Todolist() {
-  const [todo, setTodo] = useState({ desc: "", date: "", priority: "" });
+  const [todo, setTodo] = useState({ desc: "", date: null, priority: "" });
   const [todos, setTodos] = useState([]);
 
   const gridRef = useRef();
@@ -19,7 +24,7 @@ export default function Todolist() {
   const addTodo = (event) => {
     event.preventDefault();
     setTodos([...todos, todo]);
-    setTodo({ desc: "", date: "", priority: "" });
+    setTodo({ desc: "", date: null, priority: "" });
   };
 
   const deleteTodo = () => {
@@ -59,7 +64,6 @@ export default function Todolist() {
     {
       field: "priority",
       sortable: true,
-      filter: true,
       suppressMenu: true,
       filter: "agTextColumnFilter",
       floatingFilterComponentParams: {
@@ -75,65 +79,66 @@ export default function Todolist() {
 
   return (
     <div className="App">
-      <Stack
-        direction="row"
-        spacing={2}
-        alignItems="center"
-        justifyContent="center"
-      >
-        <TextField
-          variant="standard"
-          label="Date"
-          name="date"
-          value={todo.date}
-          onChange={inputChanged}
-        />
-        <TextField
-          variant="standard"
-          label="Description"
-          name="desc"
-          value={todo.desc}
-          onChange={inputChanged}
-        />
-        <TextField
-          variant="standard"
-          label="Priority"
-          name="priority"
-          value={todo.priority}
-          onChange={inputChanged}
-        />
-        <Button
-          size="small"
-          startIcon={<AddIcon />}
-          variant="contained"
-          onClick={addTodo}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Stack
+          direction="row"
+          spacing={2}
+          alignItems="center"
+          justifyContent="center"
         >
-          Add todo
-        </Button>
-        <Button
-          size="small"
-          startIcon={<DeleteIcon />}
-          variant="contained"
-          color="error"
-          onClick={deleteTodo}
-        >
-          Delete
-        </Button>
-      </Stack>
+          <DatePicker
+            views={["year", "month", "day"]} //to do: mahdollinen ratkaisu
+            value={todo.date}
+            onChange={(date) => setTodo({ ...todo, date })}
+          />
 
-      <div
-        className="ag-theme-material"
-        style={{ height: 500, width: 600, margin: "auto" }}
-      >
-        <AgGridReact
-          ref={gridRef}
-          onGridReady={(params) => (gridRef.current = params.api)}
-          rowData={todos}
-          columnDefs={columnDefs}
-          rowSelection="single"
-          animateRows={true}
-        ></AgGridReact>
-      </div>
+          <TextField
+            variant="standard"
+            label="Description"
+            name="desc"
+            value={todo.desc}
+            onChange={inputChanged}
+          />
+          <TextField
+            variant="standard"
+            label="Priority"
+            name="priority"
+            value={todo.priority}
+            onChange={inputChanged}
+          />
+          <Button
+            size="small"
+            startIcon={<AddIcon />}
+            variant="contained"
+            onClick={addTodo}
+          >
+            Add todo
+          </Button>
+          <Button
+            size="small"
+            startIcon={<DeleteIcon />}
+            variant="contained"
+            color="error"
+            onClick={deleteTodo}
+          >
+            Delete
+          </Button>
+        </Stack>
+
+        <div
+          className="ag-theme-material"
+          style={{ height: 500, width: 600, margin: "auto" }}
+        >
+          <AgGridReact
+            ref={gridRef}
+            onGridReady={(params) => (gridRef.current = params.api)}
+            rowData={todos}
+            columnDefs={columnDefs}
+            rowSelection="single"
+            animateRows={true}
+          ></AgGridReact>
+        </div>
+      </LocalizationProvider>
     </div>
   );
 }
